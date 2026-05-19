39dad3ad97d891e4e71f37c2bc75fe783b55f678
개발자1 <cm41823@gmail.com>
2026-05-19 11:38:12 +0900

    docs(study): Dto도입

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -1,29 +1,36 @@
 package com.back.domain.post.post.controller;
 
+import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController//@Controller + @ResponseBody
+@RestController // @Controller + @ResponseBody
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class ApiV1PostController {
     private final PostService postService;
 
     @GetMapping
-    @ResponseBody
-    public List<Post> getItems() {
+    public List<PostDto> getItems() {
         List<Post> items = postService.getList();
 
-        return items;
+        return items
+                .stream()
+                .map(post -> new PostDto(post)) // postDto 변환
+                .toList();
     }
+
     @GetMapping("/{id}")
-    public Post getItem(@PathVariable Long id) {
+    public PostDto getItem(@PathVariable Long id) {
         Post item = postService.getPost(id);
 
-        return item;
+        return new PostDto(item);
     }
 }
