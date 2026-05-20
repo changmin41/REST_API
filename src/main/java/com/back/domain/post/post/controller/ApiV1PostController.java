e1da3c68861ed871d08fad3f927c185ad5239351
개발자1 <cm41823@gmail.com>
2026-05-20 16:51:16 +0900

    feat(post): 게시글 생성 API 구현

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -1,15 +1,14 @@
 package com.back.domain.post.post.controller;
 
 import com.back.domain.post.post.dto.PostDto;
+import com.back.domain.post.post.dto.PostWriteReqBody;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
-import com.back.global.rsData.ForPostRsData;
+import com.back.global.rsData.RsData;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,12 +38,24 @@ public class ApiV1PostController {
     }
 
     @Transactional
-    @GetMapping("/{id}/delete")
-    public ForPostRsData delete(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public RsData<PostDto> delete(@PathVariable Long id) {
         Post post = postService.findById(id);
 
         postService.delete(post);
 
-        return new ForPostRsData("200-1", "%d번 게시글이 삭제되었습니다.".formatted(id), new PostDto(post));
+        return new RsData<>("200-1", "%d번 게시글이 삭제되었습니다.".formatted(id), new PostDto(post));
+    }
+
+    @Transactional
+    @PostMapping
+    public RsData<PostDto> write(@Valid @RequestBody PostWriteReqBody form) {
+        Post post = postService.create(form.title(), form.content());
+
+        return new RsData<>(
+                "200-1",
+                "%d번 게시글이 생성되었습니다.".formatted(post.getId()),
+                new PostDto(post)
+        );
     }
 }
