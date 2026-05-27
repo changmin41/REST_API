7a83ddeafb94c050036cffd039ed32cf6cf70773
개발자1 <cm41823@gmail.com>
2026-05-27 11:03:43 +0900

    feat: 홈 엔드포인트 추가

--- src/main/java/com/back/domain/home/controller/HomeController.java
+++ src/main/java/com/back/domain/home/controller/HomeController.java
@@ -1,8 +1,15 @@
 package com.back.domain.home.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+
+    @GetMapping
+    public String home(){
+        return "홈";
+    }
 
 }
